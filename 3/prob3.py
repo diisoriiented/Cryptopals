ba34b6b05a7f3e349d7fa121dbcb2f91d3297d61
Diisoriiented <Diisoriiented@gmail.com>
2017-01-13 16:23:04 -0800

    Completed problem 3, the hardest part of this question was writing the scoring algorithm for the words. I used a method of character frequency in the english language, creating an overall expected frequency according to https://en.wikipedia.org/wiki/Letter_frequency. I multipled the frequency by 100, and added them to a total expected frequency. Then, I chcked each string for its approximate frequency of random letters (uncommon and common). The one with the closest score to the total expected frequency was chosen. In addition, I excluded all strings from consideration with special characters aside from ' and a space.

---
+++ 3/prob3.py
@@ -0,0 +1,55 @@
+#!/usr/local/bin/python
+
+from binascii import *
+from sys import *
+from math import *
+
+def h2b(hex):
+    return unhexlify(hex)
+
+def b2h(bin):
+    return hexlify(bin)
+
+def xor(input, key):
+    result = ""
+    for ch in input:
+        result += chr(ord(ch) ^ key)
+    return result
+
+def str_score(str):
+    score  = ch_freq("etaozxkwfihlvpbqmjg", str)
+    score = fabs(84.7- score)
+    return score
+
+def ch_freq(ch_str, str):
+    score = 0
+    count = 0
+    leng = len(str)
+    for char in ch_str:
+        count = 0
+        for ch in str:
+            if ch not in "abcdefghijklmnopqrstuvwxyz' ": return 0
+            if char in str:
+                count += 1
+        score += count/leng
+    return score
+
+def crack(input):
+    best = 1000
+    msg = ""
+    for i in range(256):
+        result = xor(input, i)
+        score = str_score(result.lower())
+        if score < best:
+            best = score
+            msg = result
+    return msg
+
+
+def main():
+    hex = h2b(argv[1])
+    result = crack(hex)
+    print (result)
+
+
+main()
