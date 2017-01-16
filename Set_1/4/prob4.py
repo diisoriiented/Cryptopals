9aac6bbdda95a21397c163dcdb425ec68506c4a7
Diisoriiented <Diisoriiented@gmail.com>
2017-01-16 11:28:41 -0800

    Finished! Scoring algorithm wasn't working properly, not sure as to why. What I changed was excluding strings with ascii values greater than 127, everything above that number isn't an english character, and BOOM, done

--- Set_1/4/prob4.py
+++ Set_1/4/prob4.py
@@ -18,7 +18,7 @@ def xor(input, key):
 
 def str_score(str):
     score  = ch_freq("etao", str)
-    score = fabs(37.4-score)
+    score = fabs(37.4-score) #    The closer to 0, the better
     return score
 
 def ch_freq(ch_str, str):
@@ -26,17 +26,19 @@ def ch_freq(ch_str, str):
     count = 0.0
     str = str.split(' ')
     res_str = ""
+
+    #    ignores spaces in length calculation
     for string in str:
         res_str += string
     leng = len(res_str)
     for char in ch_str:
-        count = 0.0
+        count = 0.0 #   Var to calculate frequency
         for ch in res_str:
-            if ch not in "abcdefghijklmnopqrstuvwxyz' .;:?": return 0
+            if ord(ch) > 127 : return 0 #skips characters that aren't readable ascii
             # print(ch)
             if char == ch:
                 count += 1
-        score += (count/leng) * 100
+        score += (count/leng) * 100 #    Frequency calculation
     return score
 
 def crack(input):
@@ -53,17 +55,16 @@ def crack(input):
 
 def main():
     global_score = 1000
-    best = 0
     msg  = ""
     hex_file = open(argv[1], 'r').read().split('\n')
     for line in hex_file:
-        original_hex = line #   Saves original line
+        original_hex = line #   Saves original line a.k.a. the answer
         line = h2b(line) #    Converts to binary
         (result,score) = crack(line) #    Should return highest scoring binary string as well as its score
-        print(result, score)
         if score < global_score:
             msg = result
-            best = score
-    print(msg)
-    print(best)
+            global_score = score
+
+    print(msg, global_score)
+    print(original_hex)
 main()
